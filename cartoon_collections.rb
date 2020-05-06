a4f22445bb58c470f4b3e7e5e3de8c762b15eb4a
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 18:30:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,6 +26,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find { |word| word == cheese_types } 
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |word| word == cheese_types } 
 end
