0b514ec837a067cc7e5ba61888ed96d53d0dd3aa
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 18:24:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+    array.any? do |word|
+      word.length > 4
+    end
 end
 
 def find_the_cheese# code an argument here
