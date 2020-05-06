1f41b274b32e41c4e791e0962cf0b6a6705d0e44
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 18:20:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  roll = []
+  array.each_with_index do |name, index|
+    place = index + 1 
+    roll.push("#{place} #{name}")
+  end
+  puts roll 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  
+  i = 0 
+  while i < array.length 
+    new_array << array[i].capitalize + "!"
+    i +=1 
+  end 
+  
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
