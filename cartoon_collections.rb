a7e22cf3424d8cc904df237c570f63582e697591
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 18:32:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,5 +27,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each { |word| cheese_types.include?(type) } 
+  array.each { |word| cheese_types.include?(word) } 
 end
